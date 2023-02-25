2f1d64fd49e67ca99eff01e0761e4608ec902fdd
letier3110 <letier3110@gmail.com>
2023-02-25 16:34:11 +0400

    fix emoji size

--- src/components/mafia/Button.tsx
+++ src/components/mafia/Button.tsx
@@ -10,7 +10,7 @@ export const Button: FC<ButtonProps> = ({ children, className, onClick }) => {
   return (
     <button
       className={[
-        'inline-block bg-white text-gray-700 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-200 active:shadow-lg transition duration-150 ease-in-out mr-4',
+        'inline-block bg-white text-gray-700 font-medium uppercase rounded shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-100 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-200 active:shadow-lg transition duration-150 ease-in-out mr-4',
         className ?? ''
       ].join(' ')}
       role='button'
